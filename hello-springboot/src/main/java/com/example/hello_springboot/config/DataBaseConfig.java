b6b3da9c031867edcd80eb5bc7cb7bbb10ec70df
kcw9609 <kang960953@gmail.com>
2025-06-22 11:26:14 +0900

    Config: bean 등록 설정

---
+++ hello-springboot/src/main/java/com/example/hello_springboot/config/DataBaseConfig.java
@@ -0,0 +1,13 @@
+package com.example.hello_springboot.config;
+
+import com.example.hello_springboot.db.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class DataBaseConfig {
+//    @Bean
+//    public UserRepository userRepository() {
+//        return new UserRepository();
+//    }
+//}
